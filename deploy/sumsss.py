641ef1e54abf20980e6618d550a56ddf5b1a69eb
inhyuk0102 <109231492+inhyuk0102@users.noreply.github.com>
2024-08-05 03:28:45 +0900

    Update sumsss.py

--- deploy/sumsss.py
+++ deploy/sumsss.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-from PIL import Image
-from tkinter.tix import COLUMN
 from pyparsing import empty
 from streamlit_calendar import calendar
 import itertools
